71ba545d755ff383efed3a01b5daba33d2dc6bb2
Gabriela Targino <targinogabriela@gmail.com>
2023-03-01 16:36:30 -0300

    criação de componentes e da lógica do jogo

---
+++ src/components/GameOver.js
@@ -0,0 +1,12 @@
+import './GameOver.css'
+
+const Game = ({ retry }) => {
+  return (
+    <div>
+      <h1>Game Over</h1>
+      <button onClick={retry}>Resetar jogo</button>
+    </div>
+  )
+}
+
+export default Game
